f4b744057c3d1decf789094e0ba087afcf25d106
JV-Amorim <joaovitoramorimx@gmail.com>
2020-08-13 20:24:10 -0300

    Commit inicial. Primeira animação criada, onde uma tela some deslizando para um lado, enquanto a outra tela aparece deslizando a partir do lado oposto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { FirstQuestionComponent } from 'src/app/components/first-question/first-question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data: { animationState: 'One' } },
+  { path: 'first-question', component: FirstQuestionComponent, data: { animationState: 'Two' }  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FirstQuestionComponent } from './components/first-question/first-question.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FirstQuestionComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
