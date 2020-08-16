5c654fae36a80322231bcfbb3fee57af61f64d85
JV-Amorim <joaovitoramorimx@gmail.com>
2020-08-16 15:37:55 -0300

    Adicionada animação CSS nos elementos da segunda tela.

--- src/app/components/first-question/first-question.component.ts
+++ src/app/components/first-question/first-question.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-question',
   templateUrl: './first-question.component.html',
-  styleUrls: ['./first-question.component.css']
+  styleUrls: ['./first-question.component.css', './first-question-animations.css']
 })
 export class FirstQuestionComponent implements OnInit {
 
